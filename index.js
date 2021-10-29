1b0ecdc6901c1c60dd88a5708ac1f33219cf65a7
Sumonbhuiya <sumonbhuiya97@gmail.com>
2021-10-29 23:32:23 +0600

    server added

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e8ysq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('travel_agency');
+        const serviceCollection = database.collection('services');
+        const expertsCollection = database.collection('experts');
+        const useServiceCollection = database.collection('use_services');
+
+        // get api for service
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        // post api for service
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.json(result);
+        })
+
+
+        // get api for experts guide
+        app.get('/experts', async (req, res) => {
+            const cursor = expertsCollection.find({});
+            const experts = await cursor.toArray();
+            res.send(experts);
+        })
+
+        // get api for manage all orders
+        app.get('/use_services', async (req, res) => {
+            const cursor = useServiceCollection.find({});
+            const manage = await cursor.toArray();
+            res.send(manage);
+        })
+
+        // update api status 
+        app.put('/use_services/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: "Accept"
+                },
+            };
+            const result = await useServiceCollection.updateOne(filter, updateDoc, options)
+            res.json(result)
+        })
+
+        // delete api from use_service 
+        app.delete('/use_services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await useServiceCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // get single service 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        //add orders api
+        app.post('/place_order', async (req, res) => {
+            const order = req.body;
+            const result = await useServiceCollection.insertOne(order);
+            res.json(result)
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Tourism server is running ...');
+})
+
+app.listen(port, () => {
+    console.log('Server running...', port)
+})
